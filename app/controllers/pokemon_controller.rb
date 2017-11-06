ddb6d09566e63ffc5a2d60deb94a14fa8bae1b9f
Michelle Cinello <m.cinello@gmail.com>
2017-11-06 17:26:00 -0500

    can render id, name and type class of pokemon

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,9 +1,16 @@
+require 'json'
+
 class PokemonController < ApplicationController
   def show
     res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
     body = JSON.parse(res.body)
+    id = body["id"]
     name  = body["name"]
-
-    render json: { "message": "ok" }
+    types = body["types"]
+    render json: {
+      id: id,
+      name: name,
+      types: types[0]["type"]["name"]
+    }
   end
 end
