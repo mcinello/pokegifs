8261bd77587fa965c445c95f62f48ef7316c8686
Michelle Cinello <m.cinello@gmail.com>
2017-11-06 19:53:34 -0500

    added GIF url into JSON response

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,16 +1,16 @@
-require 'json'
-
 class PokemonController < ApplicationController
   def show
-    res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
-    body = JSON.parse(res.body)
-    id = body["id"]
-    name  = body["name"]
-    types = body["types"]
-    render json: {
-      id: id,
-      name: name,
-      types: types[0]["type"]["name"]
-    }
+    poke_res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
+    poke_data = JSON.parse(poke_res.body)
+    id = poke_data["id"]
+    name  = poke_data["name"]
+    types = poke_data["types"]
+
+    giphy_res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=#{name}&rating=g")
+    giphy_data = JSON.parse(giphy_res.body)
+
+    gif_url = giphy_data["data"][0]["url"]
+
+    render json: { 'id' => "#{id}", 'name' => "#{name}", 'type' => "#{types[0]["type"]["name"]}", 'gif' => "#{gif_url}"}
   end
 end
